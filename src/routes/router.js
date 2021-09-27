8705934c345d3bc9f57c9456020f8b36dd163e37
Yash Sharma <yashsharma1859@gmail.com>
2021-09-27 16:40:37 +0530

    test2

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs/dist/bcrypt");
-=======
 const bcrypt = require("bcryptjs");
->>>>>>> b0227470a639a904d1f0b4375397de16ec5d7a27
 const express = require("express");
 const router = express.Router();
 const User = require("./../models/userSchema");
@@ -11,105 +6,93 @@ const authenticate = require("../middleware/authenticate");
 
 // USER REGISTRATION ROUTE
 router.post("/register", async (req, res) => {
-  // DESTRUCTURING THE BODYDATA
-  const { name, email, usertype, password, githubusername, dribbbleusername } =
-    req.body;
+	// DESTRUCTURING THE BODYDATA
+	const { name, email, usertype, password, githubusername, dribbbleusername } =
+		req.body;
 
-  // CHECK FOR THE DATA FIELDS
-  if (!name || !email || !usertype || !password) {
-    return res.status(422).json({ message: "please fill the data properly" });
-  }
+	// CHECK FOR THE DATA FIELDS
+	if (!name || !email || !usertype || !password) {
+		return res.status(422).json({ message: "please fill the data properly" });
+	}
 
-  try {
-    //   CHECKING THAT THE USER ALREADY EXIST OR NOT
-    const userExists = await User.findOne({ email: email });
+	try {
+		//   CHECKING THAT THE USER ALREADY EXIST OR NOT
+		const userExists = await User.findOne({ email: email });
 
-    if (userExists) {
-      return res.status(422).json({ message: "email already exist" });
-    }
+		if (userExists) {
+			return res.status(422).json({ message: "email already exist" });
+		}
 
-    // REGISTERING THE USERDATA TO THE DATABASE
-    else {
-      const user = new User({
-        name,
-        email,
-        usertype,
-        password,
-        githubusername,
-        dribbbleusername,
-      });
-      // WILL GET EXECUTED AFTER THE PRE METHOD IN USERSCHEMA.JS
-      const result = await user.save();
-      res.status(201).json({ message: "user registered successfully" });
-      console.log(result);
-    }
-  } catch (err) {
-    // CATCH FOR ANY SERVER OR ANY OTHER ERROR
-    res.status(500).send(err);
-  }
+		// REGISTERING THE USERDATA TO THE DATABASE
+		else {
+			const user = new User({
+				name,
+				email,
+				usertype,
+				password,
+				githubusername,
+				dribbbleusername,
+			});
+			// WILL GET EXECUTED AFTER THE PRE METHOD IN USERSCHEMA.JS
+			const result = await user.save();
+			res.status(201).json({ message: "user registered successfully" });
+			console.log(result);
+		}
+	} catch (err) {
+		// CATCH FOR ANY SERVER OR ANY OTHER ERROR
+		res.status(500).send(err);
+	}
 });
 
 // LOGIN ROUTE
 router.post("/login", async (req, res) => {
-  // DESTRUCTURING THE VARIABLES
-  const { email, password } = req.body;
+	// DESTRUCTURING THE VARIABLES
+	const { email, password } = req.body;
 
-  // CHEKCING FOR THE DATAFIELDS
-  if (!email || !password) {
-    return res.status(400).json({ message: "fill the data properly" });
-  }
-  try {
-    // GETTING THE DATA FORM DATABASE
-    const usercheck = await User.findOne({ email: email });
+	// CHEKCING FOR THE DATAFIELDS
+	if (!email || !password) {
+		return res.status(400).json({ message: "fill the data properly" });
+	}
+	try {
+		// GETTING THE DATA FORM DATABASE
+		const usercheck = await User.findOne({ email: email });
 
-    // CHECK FOR THE USER EXISTANCE
-    if (usercheck) {
-      // CHECKING FOR THE PASSWORD
-      const isMatch = await bcrypt.compare(password, usercheck.password);
+		// CHECK FOR THE USER EXISTANCE
+		if (usercheck) {
+			// CHECKING FOR THE PASSWORD
+			const isMatch = await bcrypt.compare(password, usercheck.password);
 
-      // GENERATING AUTHORIZATION TOKEN
-      const token = await usercheck.generateAuthToken();
+			// GENERATING AUTHORIZATION TOKEN
+			const token = await usercheck.generateAuthToken();
 
-<<<<<<< HEAD
-      // STORING TOKEN TO COOKIE
-      await res.cookie("jwtoken", token, {
-        expires: new Date(Date.now() + 200000000000),
-        httpOnly: true,
-      });
+			console.log("token in login : ", token);
 
-      // CHECKING FOR USER CREDENTIALS
-      if (usercheck && email == usercheck.email && isMatch) {
-        return res.status(200).json({ message: "Login successfully" });
-=======
-      console.log("token in login : ", token);
+			// CHECKING FOR USER CREDENTIALS
+			if (usercheck && email == usercheck.email && isMatch) {
+				// SENDING THE ACCESSTOKEN TO FRONTEND WITH RESPONSE DATA
+				return res
+					.status(200)
+					.json({ message: "Login successfully", accessToken: token });
+			} else {
+				// IF PASSWORD IS NOT SAME
+				return res.status(400).json({ message: "invalid credentials" });
+			}
+		} else {
+			// IF THE USER IS NOT REGISTERED
+			return res.status(400).json({ message: "user is not registred" });
+		}
 
-      // CHECKING FOR USER CREDENTIALS
-      if (usercheck && email == usercheck.email && isMatch) {
-        // SENDING THE ACCESSTOKEN TO FRONTEND WITH RESPONSE DATA
-        return res
-          .status(200)
-          .json({ message: "Login successfully", accessToken: token });
->>>>>>> b0227470a639a904d1f0b4375397de16ec5d7a27
-      } else {
-        // IF PASSWORD IS NOT SAME
-        return res.status(400).json({ message: "invalid credentials" });
-      }
-    } else {
-      // IF THE USER IS NOT REGISTERED
-      return res.status(400).json({ message: "user is not registred" });
-    }
-
-    // IN CASE OF INVALID USERID OR PASSWORD
-  } catch (error) {
-    //CATCH BLOCK
-    return res.status(400).json({ message: error });
-  }
+		// IN CASE OF INVALID USERID OR PASSWORD
+	} catch (error) {
+		//CATCH BLOCK
+		return res.status(400).json({ message: error });
+	}
 });
 
 // HOME DASHBOARD PAGE
 router.get("/dashboard", authenticate, (req, res) => {
-  res.send(req.rootUser);
-  console.log("this is rootuser dashboard");
+	res.send(req.rootUser);
+	console.log("this is rootuser dashboard");
 });
 
 module.exports = router;

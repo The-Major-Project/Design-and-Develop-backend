d164ee6593b40d3974f7055d1beedc9d8151a4a0
Mihir Verma <mihirv7781@gmail.com>
2021-11-11 12:00:13 +0530

    added all post apis

--- src/models/postSchema.js
+++ src/models/postSchema.js
@@ -1,17 +1,37 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-});
+const PostSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    require: true,
+  },
+  title: {
+    type: String,
+    max: 100,
+  },
+  description: {
+    type: String,
+    max: 500,
+  },
+  developer:{
+	type: Number,
+    min: 0,
+	required: true,
+  },
+  designer:{
+	type: Number,
+    min: 0,
+	required: true,
+  },
+  img: {
+    type: String,
+  },
+  likes:{
+      type: Array,
+      default:[]
+  }
+},
+{ timestamps: true }
+);
 
-// CREATING A MODEL FOR THE SCHEMA
-const Post = mongoose.model("POST", postSchema);
-
-module.exports = Post;
+module.exports = mongoose.model("Post", PostSchema);

061f812685f7261ca1f524cc10f37c797edd3299
Yash Sharma <yashsharma1859@gmail.com>
2021-11-23 03:44:57 +0530

    NotRealTimeChat

--- src/models/groupSchema.js
+++ src/models/groupSchema.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 
 const GroupSchema = new mongoose.Schema(
-  {
-    groupId: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    groupName: {
-      type: String,
-      required: true,
-    },
-    members: {
-      type: Array,
-    },
-  },
-  { timestamps: true }
+	{
+		groupId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		groupName: {
+			type: String,
+			required: true,
+		},
+		members: {
+			type: Array,
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("Group", GroupSchema);
